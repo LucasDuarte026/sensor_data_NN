450626d3b0dd739b7cb2a55278c39d56b90a1c49
LucasDuarte026 <lucas.sales@usp.br>
2023-02-24 03:00:45 -0300

    model_mlp funcionando perfeitamente

--- pytorch_in/src/testador.py
+++ pytorch_in/src/testador.py
@@ -1,8 +1,10 @@
-
 import numpy as np
+import pandas as pd
 
-test = np.empty([2,2])
-print(test)
-for i in range(100):
-    test = np.append(test,[i,i*2])
-print(test[0])
+df = pd.read_csv(
+    '/mnt/c/Users/micro/OneDrive/Documentos/Faculdade/neural_network_2023/graph_images/grandão/dados_criados.dat', sep="\s+")
+diff = (df.predicted - df.original)
+diff_media = diff.abs().mean()
+diff_max=diff.abs().max()
+diff_min=diff.abs().min()
+print(f'media da diferença: {diff_media:6.6f}\nmáxima diferença:    {diff_max:6.6f}\nMínima diferença:   {diff_min:6.6f}')
