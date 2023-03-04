40e1b9bb1bfda0cfc2819b9230fb1603ca9ee286
LucasDuarte026 <lucas.sales@usp.br>
2023-03-04 18:53:38 -0300

    Começo da novo script

---
+++ pytorch_in/src/past/working.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from matplotlib import pyplot as plt
+
+EPOCHS = 200
+input_size = 1
+output_size = 1
+hidden_layers = 3
+hidden_size = 32
+learning_rate = 0.01
+batch_size = 32
+num_epochs = 100
+amount = 120000
+SAVE = False
+
+# Carregamento dos dados para dentro da rede
+voltage_fake = pd.read_csv(
+    '/mnt/c/Users/micro/OneDrive/Documentos/Faculdade/ic_neural_network/nn_ic/air_analysis/data/hotfilm-fake-21180.dat', sep="\s+")
+velo_fake = pd.read_csv(
+    '/mnt/c/Users/micro/OneDrive/Documentos/Faculdade/ic_neural_network/nn_ic/air_analysis/data/hotfilm-fake-vel-21180.dat', sep="\s+")
+data = voltage_fake.assign(
+    velocity=velo_fake['velocity']).assign(time=velo_fake['time'])
+data = data[['voltage', 'velocity']]
+data = data.head(amount)
+
+print(data.head(amount))
+
+# Define the MLP architecture as a PyTorch module
+
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim=64, num_hidden_layers=2):
+        super(MLP, self).__init__()
+        self.input_layer = nn.Linear(input_dim, hidden_dim)
+        self.hidden_layers = nn.ModuleList()
+        for i in range(num_hidden_layers):
+            self.hidden_layers.append(nn.Linear(hidden_dim, hidden_dim))
+        self.output_layer = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.input_layer(x))
+        for layer in self.hidden_layers:
+            x = torch.relu(layer(x))
+        x = self.output_layer(x)
+        return x
+
+
+# Extract the input and output variables as PyTorch tensors
+X = torch.tensor(data['voltage'].values).float().unsqueeze(1)
+Y = torch.tensor(data['velocity'].values).float().unsqueeze(1)
+
+# Define the MLP and loss function
+mlp = MLP(input_dim=1, output_dim=1, hidden_dim=64, num_hidden_layers=2)
+criterion = nn.MSELoss()
+
+# Define the optimizer and learning rate
+optimizer = optim.Adam(mlp.parameters(), lr=0.01)
+
+# Train the MLP on the entire dataset
+for epoch in range(EPOCHS):
+    # Forward pass
+    outputs = mlp(X)
+    loss = criterion(outputs, Y)
+
+    # Backward pass and optimization
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Print progress
+    if epoch % 100 == 0:
+        print("Epoch {}, loss {:.4f}".format(epoch, loss.item()))
+
+# Evaluate the MLP on the entire dataset
+with torch.no_grad():
+    predictions = mlp(X)
+    accuracy = ((predictions - Y) ** 2).mean().sqrt().item()
+    print("Test accuracy| Mean Loss: {:.4f}".format(accuracy))
+
+
+# Showing data
+shown = pd.DataFrame(predictions.detach().numpy())
+shown = shown.assign(original=data['velocity'])
+pd.set_option('display.max_rows', None)
+print(shown)
+
+# Plotting both the curves simultaneously
+plt.plot(data.voltage, data.velocity, color='r', label='data')
+plt.plot(data.voltage, predictions.detach().numpy(),
+        color='g', label='processed')
+
+# Naming the x-axis, y-axis and the whole graph
+plt.xlabel("Voltage")
+plt.ylabel("Velocity")
+plt.title("Gráfico com a relação entre Tensão e Velocidade")
+
+# Adding legend, which helps us recognize the curve according to it's color
+plt.legend()
+
+# To load the display window
+plt.show()
+
+# Save the trained MLP to a file
+torch.save(mlp.state_dict(), "mlp.pth")
